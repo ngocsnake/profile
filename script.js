d90d3cbb6257bcabfe435768231c3ab81c97ea06
ngocsnake <ngocsnake@gmail.com>
2022-03-11 01:14:25 +0700

    first commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const shuffleArray = (arr) => {
     return output;
 }
 
-const icons = ['🥗', '🍩', '🍵', '🥝', '🍉', '🌷', '🎊', '💖']
+const icons = ['🥗', '🍩', '🍵', '🥪', '🌳', '🌷', '🎊', '💖']
 const arrayID = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]
 
 const arrayRandom = shuffleArray(arrayID);
